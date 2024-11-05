5890b2c6de459d9241fd54b46fcda504aa98fb0e
Aaron Santiago <asantia727@gmail.com>
2024-11-05 14:52:11 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,45 +1,41 @@
 class Vehicle {
-  status = "stopped";
-
-  constructor(make, model, wheels) {
-    this.make = make;
-    this.model = model;
-    this.wheels = wheels;
-  }
-  start() {
-    this.status = "started";
-  }
-  stop() {
-    this.status = "stopped";
-  }
+    constructor(make, model, wheels) {
+        this.make = make;
+        this.model = model;
+        this.wheels = wheels;
+        this.status = "stopped";
+    }
+    start() {
+        this.status = "started";
+    }
+    stop() {
+        this.status = "stopped";
+    }
 }
-
 class Car extends Vehicle {
-  constructor(make, model) {
-    super(make, model, "four");
-  }
+    constructor(make, model) {
+        super(make, model, 4);
+    }
 }
-
 class MotorCycle extends Vehicle {
-  constructor(make, model) {
-    super(make, model, 2);
-  }
+    constructor(make, model) {
+        super(make, model, 2);
+    }
 }
-
 function printStatus(vehicle) {
-  if (vehicle.status === "running") {
-    console.log("The vehicle is running.");
-  } else {
-    console.log("The vehicle is stopped.");
-  }
+    if (vehicle.status === "running") {
+        console.log("The vehicle is running.");
+    }
+    else {
+        console.log("The vehicle is stopped.");
+    }
 }
-
 const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
-console.log(myHarley.make.toUppercase());
-
+console.log(myHarley.make.toUpperCase());
 const myBuick = new Car("Buick", "Regal");
-myBuick.wheels = myBuick.wheels - 1;
+myBuick.wheels = myBuick.wheels;
+-1;
 console.log(myBuick.wheels);
-console.log(myBuick.mdl);
+console.log(myBuick.model);
